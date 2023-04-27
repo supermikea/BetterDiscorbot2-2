f1d7f10952f28633330d9bdf4d6ae86ef855cdab
moik <mike12wiegman@gmail.com>
2023-04-27 16:21:05 +0200

    accepting it is broken

--- src/developer/developer.py
+++ src/developer/developer.py
@@ -6,7 +6,7 @@ import nextcord
 from nextcord.ext import commands, tasks
 
 # global vars
-sys.stdout = open('log.log', 'w')
+# sys.stdout = open('log.log', 'w')
 
 
 class developer(commands.Cog):
@@ -34,11 +34,13 @@ class developer(commands.Cog):
     @commands.command()
     async def get_log(self, ctx):
         if str(ctx.author) == "supermikea#5051":
-            await ctx.reply("Sure Mike!\n Uploading log...")
-            with open('log.log', 'rb') as f:
-                log_file = nextcord.File(fp=f, filename="log.txt", description="your log mike")
-            await ctx.send(file=log_file)
-            sys.stdout = open('log.log', 'w')
+            await ctx.reply("it broken sooooooooooo no...")
+            
+            # await ctx.reply("Sure Mike!\n Uploading log...")
+            # with open('log.log', 'rb') as f:
+            #     log_file = nextcord.File(fp=f, filename="log.txt", description="your log mike")
+            # await ctx.send(file=log_file)
+            #sys.stdout = open('log.log', 'w')
         else:
             await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
 

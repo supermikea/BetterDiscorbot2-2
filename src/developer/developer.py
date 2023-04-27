888f0d666fb0d226b52271dc92d4ecced4d261bf
moik <mike12wiegman@gmail.com>
2023-04-27 15:38:59 +0200

    fix restart

--- src/developer/developer.py
+++ src/developer/developer.py
@@ -12,8 +12,7 @@ class developer(commands.Cog):
 
     @commands.command()
     async def restart(self, ctx):
-        print(ctx.author)
-        if ctx.author == "supermikea#5051":
+        if str(ctx.author) == "supermikea#5051":
             os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
         else:
             await ctx.send("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")

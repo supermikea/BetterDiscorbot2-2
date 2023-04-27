e1ff3f0239e175a5873a8fcb790dcea646da3820
moik <mike12wiegman@gmail.com>
2023-04-27 15:43:52 +0200

    better devloper commands and update command added

--- src/developer/developer.py
+++ src/developer/developer.py
@@ -13,6 +13,16 @@ class developer(commands.Cog):
     @commands.command()
     async def restart(self, ctx):
         if str(ctx.author) == "supermikea#5051":
-            os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)
+            await ctx.reply("Sure Mike!\n restarting...")
+            os.execl(sys.executable, os.path.abspath(__file__), "restarted")
         else:
-            await ctx.send("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
+            await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
+    
+    @commands.command()
+    async def update(self, ctx):
+        if str(ctx.author) == "supermikea#5051":
+            await ctx.reply("Sure Mike!\n updating...")
+            output = os.system("git pull")
+            await ctx.reply("Updated! here is the output:\n" + output)
+        else:
+            await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")

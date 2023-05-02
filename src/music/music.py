466e56997387799936879ea4bcaf3a033037f806
moik <mike12wiegman@gmail.com>
2023-05-02 19:38:04 +0200

    fix audio???

--- src/music/music.py
+++ src/music/music.py
@@ -23,7 +23,7 @@ b1 = None
 # youtube_dl.utils.bug_reports_message = lambda: ""
 
 ytdl_format_options = {
-    "format": "bestaudio/best",
+    "format": "m4a/bestaudio/best",
     "outtmpl": "%(extractor)s-%(id)s-%(title)s.%(ext)s",
     "restrictfilenames": True,
     "noplaylist": False,
@@ -36,7 +36,7 @@ ytdl_format_options = {
     "source_address": "0.0.0.0",  # bind to ipv4 since ipv6 addresses cause issues sometimes
     'postprocessors': [{  # Extract audio using ffmpeg
         'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',}]
+        'preferredcodec': 'm4a',}]
 }
 
 ffmpeg_options = {"options": "-vn", 'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5'}

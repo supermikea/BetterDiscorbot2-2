ca77a660e0b0284158fc158516b1478fc367a305
moik <mike12wiegman@gmail.com>
2023-05-02 16:07:50 +0200

    update system better

--- src/developer/developer.py
+++ src/developer/developer.py
@@ -1,6 +1,6 @@
 import asyncio
-import os
 import sys
+import subprocess
 
 import nextcord
 from nextcord.ext import commands, tasks
@@ -17,7 +17,7 @@ class developer(commands.Cog):
     async def restart(self, ctx):
         if str(ctx.author) == "supermikea#5051":
             await ctx.reply("Sure Mike!\n restarting...")
-            os.system("python3 \"/home/ubuntu/BetterDiscorbot2-2/src/main.py\"")
+            subprocess.run("python3 \"/home/ubuntu/BetterDiscorbot2-2/src/main.py\"", capture_output=True)
             sys.exit()
         else:
             await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
@@ -26,11 +26,18 @@ class developer(commands.Cog):
     async def update(self, ctx):
         if str(ctx.author) == "supermikea#5051":
             await ctx.reply("Sure Mike!\n updating...")
-            output = os.system("git pull")
-            # await ctx.reply("Updated! here is the output:\n" + output.)
+            output = subprocess.run("git pull", capture_output=True)
+            await ctx.reply("Updated? here is the output:\n" + output)
         else:
             await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
     
+
+    @commands.command()
+    async def live_update(self, ctx):
+        if str(ctx.author) == "supermikea#5051":
+            await ctx.reply("TODO: Not implemented!")
+        else:
+            await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
     @commands.command()
     async def get_log(self, ctx):
         if str(ctx.author) == "supermikea#5051":

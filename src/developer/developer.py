0c4dd69ddf0044dcaead09afa06e66395bfc225a
supermikea <mike12wiegman@gmail.com>
2023-07-16 20:40:29 +0200

    fixed username and added log function

--- src/developer/developer.py
+++ src/developer/developer.py
@@ -2,12 +2,14 @@ import asyncio
 import sys
 import subprocess
 import os
+import time
 from multiprocessing import Process
 
 import nextcord
 from nextcord.ext import commands, tasks
 
 import json
+
 
 # global vars
 # sys.stdout = open('log.log', 'w')
@@ -17,53 +19,57 @@ class developer(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command()
+    @commands.command(hidden=True)
     async def restart(self, ctx):
-        if str(ctx.author) == "supermikea":
+        if str(ctx.author) == "522450312049590276":
             await ctx.reply("Sure Mike!\n restarting...")
             os.system("python3 \"/home/ubuntu/BetterDiscorbot2-2/src/main.py\" &")
             sys.exit()
         else:
             await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
-    
-    @commands.command()
+
+    @commands.command(hidden=True)
     async def update(self, ctx):
-        if str(ctx.author) == "supermikea":
+        if str(ctx.author) == "522450312049590276":
             await ctx.reply("Sure Mike!\n updating...")
-            output = subprocess.run(["git","pull"], capture_output=True).stdout
+            output = subprocess.run(["git", "pull"], capture_output=True).stdout
             await ctx.reply("Updated? here is the output:\n" + output.decode("utf-8"))
         else:
             await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
-    
 
-    @commands.command()
+    @commands.command(hidden=True)
     async def live_update(self, ctx):
-        if str(ctx.author) == "supermikea":
+        if str(ctx.author.id) == "522450312049590276":
             await ctx.reply("TODO: Not implemented!")
-            #await ctx.reply("hooking onto the latest player instance...")
-            #with open("../live_config.json", "rw") as f:
+            # await ctx.reply("hooking onto the latest player instance...")
+            # with open("../live_config.json", "rw") as f:
             #    data = json.load(f)
             #    to_write = {
             #        "live_update": True
             #    }
 
-                # merge the dictionaries
+            # merge the dictionaries
             #    to_write = data | to_write
 
         else:
             await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
-    @commands.command()
+
+    @commands.command(hidden=True)
     async def get_log(self, ctx):
-        if str(ctx.author) == "supermikea":
-            await ctx.reply("it broken sooooooooooo no...")
-            
-            # await ctx.reply("Sure Mike!\n Uploading log...")
-            # with open('log.log', 'rb') as f:
-            #     log_file = nextcord.File(fp=f, filename="log.txt", description="your log mike")
-            # await ctx.send(file=log_file)
-            #sys.stdout = open('log.log', 'w')
+        if str(ctx.author.id) == "522450312049590276":
+
+            await ctx.reply("Sure Mike!\n Uploading log...")
+            with open('log.log', 'rb') as file:
+                log_file = nextcord.File(file, filename="log.txt", description="your log mike")
+            await ctx.send(file=log_file)
         else:
             await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
+
+    @staticmethod
+    async def on_command_error(self, ctx, error):
+        await ctx.reply(f"I encountered a error during operation. at {time.time}")
+        async with open("log.log", "w") as log:
+            log.write(f"\n{ctx.author} said in {ctx.channel} at {time.time()} {ctx.message} which gave {error}")
 
 if __name__ == "__main__":
     os.system('python3 "../main.py"')

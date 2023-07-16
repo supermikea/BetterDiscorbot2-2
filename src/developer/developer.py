58751b83d99cde01a54a206f307771b8e0b3b503
supermikea <mike12wiegman@gmail.com>
2023-07-16 20:45:45 +0200

    fixewd username again

--- src/developer/developer.py
+++ src/developer/developer.py
@@ -21,7 +21,7 @@ class developer(commands.Cog):
 
     @commands.command(hidden=True)
     async def restart(self, ctx):
-        if str(ctx.author) == "522450312049590276":
+        if str(ctx.author.id) == "522450312049590276":
             await ctx.reply("Sure Mike!\n restarting...")
             os.system("python3 \"/home/ubuntu/BetterDiscorbot2-2/src/main.py\" &")
             sys.exit()
@@ -30,7 +30,7 @@ class developer(commands.Cog):
 
     @commands.command(hidden=True)
     async def update(self, ctx):
-        if str(ctx.author) == "522450312049590276":
+        if str(ctx.author.id) == "522450312049590276":
             await ctx.reply("Sure Mike!\n updating...")
             output = subprocess.run(["git", "pull"], capture_output=True).stdout
             await ctx.reply("Updated? here is the output:\n" + output.decode("utf-8"))
@@ -54,26 +54,26 @@ class developer(commands.Cog):
         else:
             await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
 
-    @commands.command(hidden=True)
-    async def get_log(self, ctx):
-        if str(ctx.author.id) == "522450312049590276":
+    #@commands.command(hidden=True)
+    #async def get_log(self, ctx):
+    #    if str(ctx.author.id) == "522450312049590276":
 
-            await ctx.reply("Sure Mike!\n Uploading log...")
-            with open('log.log', 'rb') as file:
-                log_file = nextcord.File(file, filename="log.txt", description="your log mike")
-            await ctx.send(file=log_file)
-        else:
-            await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
+    #        await ctx.reply("Sure Mike!\n Uploading log...")
+    #        with open('log.log', 'rb') as file:
+    #            log_file = nextcord.File(file, filename="log.txt", description="your log mike")
+    #        await ctx.send(file=log_file)
+    #    else:
+    #        await ctx.reply("https://tenor.com/view/no-way-dude-no-oh-bugs-bunny-bugs-gif-22941840")
 
-    @staticmethod
-    async def on_command_error(self, ctx, error):
-        await ctx.reply(f"I encountered a error during operation. at {time.time}")
-        async with open("log.log", "w") as log:
-            log.write(f"\n{ctx.author} said in {ctx.channel} at {time.time()} {ctx.message} which gave {error}")
+    #@staticmethod
+    #async def on_command_error(self, ctx, error):
+    #    await ctx.reply(f"I encountered a error during operation. at {time.time}")
+    #    async with open("log.log", "w") as log:
+    #        log.write(f"\n{ctx.author} said in {ctx.channel} at {time.time()} {ctx.message} which gave {error}")
 
-    @commands.command(hidden=True)
-    async def create_error(self, ctx):
-        await ctx.repley(0 / 0)
+    #@commands.command(hidden=True)
+    #async def create_error(self, ctx):
+    #    await ctx.reply(0 / 0)
 
 
 if __name__ == "__main__":

15bbe117f206a2616b33990db92c0acd7bee28ed
noonehas <silvijn.vankomen@gmail.com>
2024-09-19 19:35:36 +0200

    random stuffs

--- src/Economy/economy.py
+++ src/Economy/economy.py
@@ -7,30 +7,47 @@ from src.Utils import utils
 
 import random
 
-economyData = {}
+
+def log(prefix, message):
+    utils.log(prefix, "economy", message)
+
 
 class Economy(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
+
+        # get the economy data
         global economyData
-        economyData = open("Economy/economy.json", "r").read()
-        if economyData == "":
-            self.log("warning", "no economy data found")
+        try:  # open economy.json and account for all the errors
+            economyData = open("economy.json", "r").read()
+        except FileNotFoundError as e:
+            log("warning", "economy.json not found, continuing with empty economyData")
+            economyData = {}
+        except Exception as e:
+            log("warning", "error occured while opening economy.json, continuing with empty economyData")
+            economyData = {}
+
+        if economyData == "":  # load economy.json contents into dict
+            log("warning", "economy.json empty, continuing with empty economyData")
             economyData = {}
         else:
-            economyData = json.loads(economyData)
+            try:
+                log("debug", "loading economyData.json")
+                economyData = dict(json.loads(economyData))  # if everything goes well this should execute
+            except Exception as e:
+                log("warning", f"could not parse economyData, continuing with empty economyData\n{e}")
+                economyData = {}
+        log("debug", type(economyData))
+        # --
 
-    def log(prefix, message):
-        utils.log(prefix, "economy", message)
-
-    #checks if user exists in economy data
-    def check_user(self, username):
+    def check_user(self, username):  # checks if user exists in economy data
         if not economyData.__contains__(username):
             economyData[username] = {"money": 0}
 
-    def add_money(self, username, amount):
+    def add_money(self, username, amount):  # adds money to specified account (this shouldn't need a comment)
         self.check_user(username)
         economyData[username]['money'] += amount
+        open("economy.json", 'w').write(json.dumps(str(economyData)))
 
     @nextcord.slash_command(description="give coins to other user")
     async def transact(self, interaction: nextcord.Interaction):
@@ -42,6 +59,11 @@ class Economy(commands.Cog):
         amount = random.randint(0, 15)
         self.add_money(interaction.user.name, amount)
         await interaction.send(f"You found ${amount}!")
+
+    @nextcord.slash_command(description="check how much money you have")
+    async def check_money(self, interaction: nextcord.Interaction):
+        money = economyData[interaction.user.name]['money']
+        await interaction.send(f"You currently have ${money}")
 
 
 

a173bb44fa5bc52a1d81f497ee515e88bf7ffbb2
Supermikea <mike12wiegman@gmail.com>
2024-09-19 20:01:02 +0200

    updated utils.py to a better version, but it's still a TODO

--- src/Utils/utils.py
+++ src/Utils/utils.py
@@ -1,5 +1,19 @@
+import sys
+from enum import Enum
 
+class log(Enum):
+    NONE = -1
+    VERBOSE = 0
+    DEBUG = 1
+    INFO = 2
+    
+    def __init__(self, loglevel):
+        if not loglevel == isinstance(loglevel, log):
+            raise Exception("loglevel is not a enum from class log!")
+            sys.exit(1)
+        self.loglevel = loglevel
 
-def log(prefix, className, message):
-    print(f"[{prefix.upper()}] [{className.upper()}] {message}")
+    def Log(self, loglevel, message):
+        # TODO
+        print(f"[{self.loglevel.name}]: {message}")
 

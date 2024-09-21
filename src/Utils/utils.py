2803d11839b014ed58a647cd8b15e5b573153e35
supermikea <74504148+supermikea@users.noreply.github.com>
2024-09-21 20:13:56 +0000

    better logging shit

--- src/Utils/utils.py
+++ src/Utils/utils.py
@@ -1,6 +1,21 @@
 import sys
-from enum import Enum
 
-def log(prefix, className, message):
+class log:
+
+    def __init__(self, loglevel=0) -> None:
+        self.loglevel = loglevel
+
+    def log(self, prefix, className, message):
+        if self.loglevel == 0:
+            return
+        if prefix == "warning" or prefix == "error":
+            if not self.loglevel > 0:
+                return
+        elif prefix == "info":
+            if not self.loglevel > 1:
+                return
+        elif prefix == "verbose" or prefix == "debug":
+            if not self.loglevel > 2:
+                return
         print(f"[{prefix.upper()}] [{className.upper()}]: {message}")
 
